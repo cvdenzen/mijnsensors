612b63e8b9829b6d4b5a1fe6228d840803e30891
Carl van Denzen <carl@vandenzen.nl>
2018-10-02 01:29:21 +0200

    Redelijk werkende maar nog niet complete blueprint file

---
+++ pilightMqttOsgi/src/main/java/nl/vandenzen/pilightmqttosgi/util/MutableLong.java
@@ -0,0 +1,18 @@
+package nl.vandenzen.pilightmqttosgi.util;
+
+public class MutableLong {
+    volatile long value;
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+
+    // Assume value is in minutes
+    public long getMilliSeconds() {
+        return 60 * 1000 * value;
+    }
+}
