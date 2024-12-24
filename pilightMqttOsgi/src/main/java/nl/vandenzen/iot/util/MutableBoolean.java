7c76fc9df615517dfc7e1bf8065af5339d218919
Carl <carl@vandenzen.nl>
2024-12-25 00:06:57 +0100

    Startup with HueStable true after some delay

--- pilightMqttOsgi/src/main/java/nl/vandenzen/iot/util/MutableBoolean.java
+++ pilightMqttOsgi/src/main/java/nl/vandenzen/iot/util/MutableBoolean.java
@@ -6,6 +6,15 @@ public class MutableBoolean {
     private static final Logger LOG = Logger.getLogger(MutableBoolean.class.getName());
     volatile boolean value;
 
+
+    public MutableBoolean() {
+        this(false);
+    }
+    public MutableBoolean(boolean value) {
+        this.value = value;
+        LOG.info(this.toString() + " created with value " + this.value);
+    }
+
     public boolean isValue() {
         return value;
     }
@@ -15,7 +24,7 @@ public class MutableBoolean {
     }
 
     public void setValue(boolean value) {
-        LOG.info(this.toString()+" changed from " + this.value + " to " + value);
+        LOG.info(this.toString() + " changed from " + this.value + " to " + value);
         this.value = value;
     }
 }
